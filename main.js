fea83797276bfb4c55857a546fd35d284b5c21c7
D.Tamilarasan <abidm26791@gmail.com>
2023-01-09 03:06:52 -0800

    js

---
+++ main.js
@@ -0,0 +1,64 @@
+let x=50,y=40;
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x%2+y));
+
+function Expreesion(a,b){
+    return c=a+b;}
+
+console.log(Expreesion(x,y=x/2-y));
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x%2-y));
+
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x+2+y));
+
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+
+console.log(Expreesion(x,y=x+2-y));
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+
+console.log(Expreesion(x,y=x-2-y));
+
+
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x-2+y));
+
+
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x-2+y));
+
+
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x/2+y));
+
+
+
+function Expreesion(a,b){
+    return c=a+b;
+}
+console.log(Expreesion(x,y=x%2+y));
+
+
